c01f0df0d41ec665288b238af566eec7b2ca6781
micposso <micposso@gmail.com>
2023-04-23 01:43:07 -0400

    create component and react fiber

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles/styles";
+
+import Computers from './canvas/Computers';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <Computers />
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
